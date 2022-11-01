99958827212bf81daab56b58a7abf211fd4141e3
henry <henryshwong@outlook.com>
2022-11-01 10:20:29 +0800

    2

---
+++ src/people/PeopleClass.java
@@ -0,0 +1,33 @@
+package people;
+
+/*
+   Define a abstract People Class with name, sex, color, brithday and methods, lang
+      - abstract method sayHi
+      - constructor : People(name, sex, color)
+
+   Define a Student class extends People with student_id, school name, and subjects
+      - methods : sayHi, sayStudies
+      - constructor Student(name, sex, color)
+      - setter and getter  of student_id, school_name and subjects
+
+   Define an Employee class extends People with emp_id, company name and position
+      - methods : sayHi, sayPostiion
+      - constructor; Employee(name, sex, color)
+      - setter and getter of emp_id, company and position
+
+   Override method sayHi of People.
+   for Student, say: Hi I am "name", a Student, School namd and subjects
+   for Employee, say: Hi I am "name", a Employee, Company and position
+
+   Write a function to add people with different classes and print their contents.
+
+   ** use ArrayLis
+
+
+
+
+ */
+
+
+public class PeopleClass {
+}
